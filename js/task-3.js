82eb5584fc680cc14567cf0557e6e75439f32e96
achoma <139071702+achoma@users.noreply.github.com>
2024-08-16 00:39:26 +0200

    task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,45 +1,46 @@
-// Napisz funkcję strzałkową sortByDescendingFriendCount(users),
-//     która przyjmuje jeden parametr users — tablicę obiektów użytkowników.
-// Funkcja powinna zwrócić tablicę wszystkich użytkowników posortowanych 
-// według liczby znajomych w porządku malejącym(właściwość friends).
+const sortByDescendingFriendCount = (users) => {
+  return users.toSorted((first, second) =>
+    second.friends.localeCompare(first.friends)
+  );
+};
 
 console.log(
   sortByDescendingFriendCount([
     {
       name: "Moore Hensley",
       friends: ["Sharron Pace"],
-      gender: "male"
+      gender: "male",
     },
     {
       name: "Sharlene Bush",
       friends: ["Briana Decker", "Sharron Pace"],
-      gender: "female"
+      gender: "female",
     },
     {
       name: "Ross Vazquez",
       friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-      gender: "male"
+      gender: "male",
     },
     {
       name: "Elma Head",
       friends: ["Goldie Gentry", "Aisha Tran"],
-      gender: "female"
+      gender: "female",
     },
     {
       name: "Carey Barr",
       friends: ["Jordan Sampson", "Eddie Strong"],
-      gender: "male"
+      gender: "male",
     },
     {
       name: "Blackburn Dotson",
       friends: ["Jacklyn Lucas", "Linda Chapman"],
-      gender: "male"
+      gender: "male",
     },
     {
       name: "Sheree Anthony",
       friends: ["Goldie Gentry", "Briana Decker"],
-      gender: "female"
-    }
+      gender: "female",
+    },
   ])
 );
 // [
